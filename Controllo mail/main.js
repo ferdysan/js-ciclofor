746fd35e7461835cf95ee4744f59f477640e2531
Ferdysan <ferdysan87@gmail.com>
2019-04-22 08:34:21 +0200

    fine

--- Controllo mail/main.js
+++ Controllo mail/main.js
@@ -6,6 +6,6 @@ var utente = prompt('Inserisci la tua mail');
 //creo ciclo for per controllo array e stampo messaggio affermatito in caso di risultato corretto
 for (var i=0; i<mail.lenght; i++){
      if (utente == mail[i]) {
-       console.log('indirizzo mail è corretto');
+       document.writeln('indirizzo mail è corretto');
      }
 }
